cfa6c72f21dc527c1ef598a997572cb3a0d99bce
smmakowski <makowski.sm@gmail.com>
2017-02-22 13:23:34 -0800

    Finished up to and including Event Handlers

--- app.js
+++ app.js
@@ -1,1 +1,34 @@
-// TODO
+var GroceryListItem = (props) => {
+	var onListItemClick = (event) => {
+		console.log('I got clicked');
+	};
+
+	return (
+	<ul>
+		<li onClick={onListItemClick}>{props.items[0]}</li>
+		<li>{props.items[1]}</li>
+		<li>{props.items[2]}</li>
+	</ul>
+	);
+}
+
+var GroceryList = () => (
+	<div>
+	<h2>sfkjsadkf</h2>
+		<GroceryListItem items={['lettuce', 'tommato', 'onions']} />
+	</div>
+); 
+
+
+
+// var Cucumber = () => (
+// 	<li>Cucumber</li>
+// );
+
+// var Kale = () => (
+// 	<li>Kale</li>
+// );
+
+
+
+ReactDOM.render(<GroceryList />, document.getElementById('app'));
